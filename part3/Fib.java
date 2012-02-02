e485194505a668f0e72aeec93c6b381989a0dff9
Ritesh Patel <ritesh88@gmail.com>
2012-02-01 23:38:25 -0800

    Part 3 working.

---
+++ part3/Fib.java
@@ -0,0 +1,39 @@
+
+public class Fib extends Seq {
+	
+	// members
+	protected int first1, first2, last;
+	protected static int Count = 0;
+	
+	// constructor
+	public Fib(int first1, int first2, int last)
+	{
+		this.first1 = first1;
+		this.first2 = first2;
+		this.last = last;
+		
+		Count++;
+	}
+	
+	//---------------
+	//    Methods
+	//---------------
+	
+	// find upper bound
+	public int upperBound()
+	{
+		return last;
+	}
+	
+	// return the number of instances
+	public static int getCount()
+	{
+		return Count;
+	}
+	
+	// returns string
+	public String toString()
+	{
+		return "< " + first1 + ", " + first2 + " to " + last+ " >";
+	}
+}
---
+++ part3/Seq.java
@@ -0,0 +1,25 @@
+// the Seq base class
+
+public abstract class Seq {
+	
+	// members
+	protected static int Count = 0;
+	
+	public Seq()
+	{
+		Count++;
+	}
+	
+	//----------------------
+	//    Abstract Methods
+	//----------------------
+	public abstract int upperBound();
+	
+	//---------------
+	//    Methods
+	//---------------
+	public static int getCount()
+	{
+		return Count;
+	}
+}
