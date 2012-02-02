ff188c0a122e3887f8186ca27efa28add885b6b4
Ritesh Patel <ritesh88@gmail.com>
2012-02-01 22:28:53 -0800

    Part 2 working.

---
+++ part2/Fib.java
@@ -0,0 +1,26 @@
+
+public class Fib extends Seq {
+	
+	// members
+	protected int first1, first2, last;
+	
+	// constructor
+	public Fib(int first1, int first2, int last)
+	{
+		this.first1 = first1;
+		this.first2 = first2;
+		this.last = last;
+	}
+	
+	// Methods
+	public int upperBound()
+	{
+		return last;
+	}
+	
+	// returns string
+	public String toString()
+	{
+		return "< " + first1 + ", " + first2 + " to " + last+ " >";
+	}
+}
---
+++ part2/For.java
@@ -0,0 +1,26 @@
+
+public class For extends Seq {
+	
+	// members
+	protected int first, last, step;
+	
+	// constructor
+	public For(int first, int last, int step)
+	{
+		this.first = first;
+		this.last = last;
+		this.step = step;
+	}
+	
+	// Methods
+	public int upperBound()
+	{
+		return (step>0) ? last : first;
+	}
+	
+	// returns string
+	public String toString()
+	{
+		return "{ " + first + " to " + last + " by " + step + " }";
+	}
+}
